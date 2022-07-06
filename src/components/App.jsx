db8d5fca55789a0bbc04398555ccdff75baa66ea
Artem Chubenko <artem.chubenko@gmail.com>
2022-07-06 14:12:56 +0300

    Added statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,91 @@
+import { Component } from 'react';
+import AdditionalData from './statistics';
+
+class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  getAdditionalInfo() {
+    return (
+      <div>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
+        <p>Total feedbacks: {this.countTotalFeedback()}</p>
+        <p>
+          Percent of positive FeedBacks:{' '}
+          {this.countPositiveFeedbackPercentage()}%
+        </p>
+      </div>
+    );
+  }
+
+  countPositiveFeedbackPercentage() {
+    return Math.round(
+      (this.state.good /
+        (this.state.good + this.state.neutral + this.state.bad)) *
+        100
+    );
+  }
+
+  countTotalFeedback() {
+    return this.state.good + this.state.neutral + this.state.bad;
+  }
+
+  render() {
+    return (
+      <div>
+        <button
+          className="button good"
+          onClick={() => {
+            this.setState({ good: this.state.good + 1 });
+            console.log(this.state);
+          }}
+        >
+          Good
+        </button>
+        <button
+          className="button neutral"
+          onClick={() => {
+            this.setState({ neutral: this.state.neutral + 1 });
+            console.log(this.state);
+          }}
+        >
+          Neutral
+        </button>
+        <button
+          className="button bad"
+          onClick={() => {
+            this.setState({ bad: this.state.bad + 1 });
+            console.log(this.state);
+          }}
+        >
+          Bad
+        </button>
+        <p className="stat_label">STATISTICS</p>
+
+        {this.state.good || this.state.neutral || this.state.bad > 0 ? (
+          this.getAdditionalInfo()
+        ) : (
+          <p>No statistics data</p>
+        )}
+      </div>
+    );
+  }
+}
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Feedback />
+      <AdditionalData
+        good={Feedback.state.good}
+        neutral={Feedback.state.neutral}
+        bad={Feedback.state.bad}
+      />
     </div>
   );
 };
---
+++ src/components/statistics.jsx
@@ -0,0 +1,15 @@
+const getAdditionalInfo = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total feedbacks: {good + neutral + bad}</p>
+      <p>
+        Percent of positive FeedBacks: {(good / (good + neutral + bad)) * 100}%
+      </p>
+    </div>
+  );
+};
+
+export default getAdditionalInfo;
